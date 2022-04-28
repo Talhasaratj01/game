fbb1b4d4924d111a5cd1878a8b980292c290ab84
Talhasaratj01 <104562780+Talhasaratj01@users.noreply.github.com>
2022-04-28 15:32:13 +0500

    TOday working on car game

---
+++ car game/Assets/car game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cargame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 0.6f;
+    public Rigidbody2D rb;
+    Vector2 Move;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move.x = Input.GetAxis("Horizontal");
+        Move.y = Input.GetAxis("Vertical");
+    }
+    void  FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Move * speed * Time.fixedDeltaTime);
+ 
+    }
+}
---
+++ car game/Assets/car.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    float speed = 0.6f;
+    public Rigidbody2D rb;
+    Vector2 Move;
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move.x = Input.GetAxis("Horizontal");
+        Move.y = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Move * speed * Time.fixedDeltaTime);
+
+    }
+}
